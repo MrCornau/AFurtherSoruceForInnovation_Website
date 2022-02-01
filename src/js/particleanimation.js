453778cc48470a6433956c7225ad9e8429257ba3
Josh Cornau <hi@joshcornau.de>
2022-02-01 22:27:05 +0100

    added fancy animation

--- src/js/particleanimation.js
+++ src/js/particleanimation.js
@@ -9,6 +9,25 @@ const Lead1 = 60;
 const Lead2 = 200;
 const Lead3 = 20;
 const Lead4 = 90;
+let position = 0;
+
+let Comments = [
+  {
+    autor: "Daniel10 20.10.2021",
+    comment: `<span class="fontColor_Dark Font-Marked">I have created </span> an application that uses artificial intelligence to help keyword before archiving or submitting to a photo agency.`,
+    link: "https://www.reddit.com/r/naturephotography/comments/lcs6hm/over_the_last_few_days_i_created_a_web_app_to/",
+  },
+  {
+    autor: "Daniel1 20.10.2021",
+    comment: `<span class="fontColor_Dark Font-Marked">I have created </span> an application that uses artificial intelligence to help keyword before archiving or submitting to a photo agency.`,
+    link: "https://www.reddit.com/r/naturephotography/comments/lcs6hm/over_the_last_few_days_i_created_a_web_app_to/",
+  },
+  {
+    autor: "Daniel2 20.10.2021",
+    comment: `<span class="fontColor_Dark Font-Marked">I have created </span> an application that uses artificial intelligence to help keyword before archiving or submitting to a photo agency.`,
+    link: "https://www.reddit.com/r/naturephotography/comments/lcs6hm/over_the_last_few_days_i_created_a_web_app_to/",
+  },
+];
 
 for (let i = 0; i < fieldParticle; i++) {
   const div = document.createElement("div");
@@ -29,7 +48,7 @@ const BackgroundAnimation = anime
   .timeline({
     targets: ".el",
     duration: 1500,
-    delay: anime.stagger(10, { grid: gridParticle, from: "first" }),
+    // delay: anime.stagger(10, { grid: gridParticle, from: "first" }),
     loop: true,
     easing: "cubicBezier(.5, .05, .1, .3)",
   })
@@ -100,17 +119,31 @@ const BackgroundAnimation = anime
         BackgroundAnimation2.restart();
         BackgroundAnimation2.play();
         console.log("yooo");
+        document.getElementById("particle-comment").innerHTML =
+          Comments[position].comment;
+        document
+          .getElementById("particle-link")
+          .setAttribute("href", Comments[position].link);
+        document.getElementById("particle-autor").innerHTML =
+          Comments[position].autor;
+        position += 1;
+        if (position >= Comments.length) {
+          position = 0;
+        }
       },
     },
     "-=600"
   )
+  .add(
+    {
+      targets: ".particle-anime-comment ",
+      opacity: 1,
+      duration: 10,
+    },
+    "-=600"
+  )
   .add({
-    targets: ".particle-anime-comment ",
-    opacity: 1,
-    duration: 10,
-  })
-  .add({
-    duration: 10000,
+    duration: 5000,
   })
   .add(
     {
@@ -121,92 +154,51 @@ const BackgroundAnimation = anime
     "-=600"
   )
   .add({
+    targets: ".particle-triangle ",
+    opacity: 0,
+    duration: 10,
+  })
+  .add({
+    easing: "easeInCubic",
     translateX: () => anime.random(-30, 30),
-    translateY: () => anime.random(-150, 150),
-    duration: 1500,
-    borderRadius: 150,
+    translateY: () => anime.random(-10, 10),
+    duration: 500,
+    borderRadius: 30,
     backgroundColor: "#dbe5e6",
     scale: () => anime.random(0.3, 0.4),
+
+    // delay: anime.stagger(3, { grid: gridParticle, from: "last" }),
+  })
+
+  .add({
+    easing: "linear",
+    translateX: () => anime.random(-30, 30),
+    translateY: () => anime.random(-30, 30),
+    duration: 1000,
     borderRadius: 150,
-    delay: anime.stagger(10, { grid: gridParticle, from: "last" }),
+    scale: () => anime.random(0.3, 0.4),
+    // delay: anime.stagger(10, { grid: gridParticle, from: "first" }),
   })
-  .add(
-    {
-      targets: ".particle-triangle ",
-      opacity: 0,
-      duration: 10,
-    },
-    "-=800"
-  )
+
   .add({
-    easing: "easeInOutQuart",
+    easing: "easeOutCubic",
     translateX: () => anime.random(-30, 30),
     translateY: () => anime.random(-150, 150),
-    duration: 1500,
+    duration: 700,
     scale: () => anime.random(0.3, 0.4),
-    delay: anime.stagger(50, { grid: gridParticle, from: "first" }),
+    // delay: anime.stagger(50, { grid: gridParticle, from: "last" }),
   })
-  .add({
-    easing: "easeInOutQuart",
-    translateX: () => anime.random(-30, 30),
-    translateY: () => anime.random(-150, 150),
-    duration: 1500,
-    scale: () => anime.random(0.3, 0.4),
-    delay: anime.stagger(50, { grid: gridParticle, from: "last" }),
-  })
-  .add({
-    easing: "easeInOutQuart",
-    translateX: () => anime.random(-30, 30),
-    translateY: () => anime.random(-150, 150),
-    duration: 1500,
-    scale: () => anime.random(0.3, 0.4),
-    delay: anime.stagger(10, { grid: gridParticle, from: "first" }),
-  })
-  .add({
-    easing: "easeInOutQuart",
-    translateX: () => anime.random(-30, 30),
-    translateY: () => anime.random(-150, 150),
-    duration: 1500,
-    scale: () => anime.random(0.3, 0.4),
-    delay: anime.stagger(60, { grid: gridParticle, from: "last" }),
-  })
+
   .add({
     easing: "spring(1, 80, 10, 0)",
     translateX: 0,
     translateY: 0,
     scale: 1,
     borderRadius: 150,
-    duration: 1500,
+    duration: 700,
     delay: anime.stagger(10, { grid: gridParticle, from: "first" }),
     scale: 1,
   });
-
-//   .add({
-//     scale: 0.5,
-//     backgroundcolParticleor: "#ff08e8",
-//     delay: anime.stagger(150, { grid: gridParticle, from: "last" }),
-//   })
-//   .add({
-//     translateX: () => anime.random(-1500, 1500),
-//     translateY: () => anime.random(-1500, 1500),
-//     scale: 0.25,
-//     delay: anime.stagger(150, { grid: gridParticle, from: "last" }),
-//   })
-//   .add({
-//     targets: title,
-//     opacity: 1,
-//     duration: 10,
-//   })
-//   .add({
-//     translateX: 0,
-//     translateY: 0,
-//     scale: 1,
-//     borderRadius: 0,
-//     backgroundcolParticleor: "#fff",
-//     duration: 2500,
-//     delay: anime.stagger(150, { grid: gridParticle, from: "first" }),
-//     endDelay: 1500,
-//   });
 
 const BackgroundAnimation2 = anime
   .timeline({
@@ -228,7 +220,7 @@ const BackgroundAnimation2 = anime
   })
   .add({
     translateX: () => anime.random(-10, 10),
-    translateY: () => anime.random(-150, 10),
+    translateY: () => anime.random(120, 210),
     delay: anime.stagger(8, { from: "last" }),
     borderRadius: ["0%", "150%"],
     easing: "easeInOutQuad",

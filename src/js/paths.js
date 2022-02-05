48ca0ea3c77188d28ff5b6c60acdbad5db896a7e
Josh Cornau <hi@joshcornau.de>
2022-02-05 22:03:07 +0100

    More SVG Paths

--- src/js/paths.js
+++ src/js/paths.js
@@ -8,6 +8,12 @@ let draw = () => {
   let timelineSquare2 = $("#timeline-square2");
   let timelineSquare3 = $("#timeline-square3");
 
+  let timelineSquare4 = $("#timeline-square4");
+
+  let timelineSquare5 = $("#timeline-square5");
+  let timelineSquare6 = $("#timeline-square6");
+  let timelineSquare6 = $("#Stagegate");
+
   let XtimelineSquare1 = timelineSquare1.offset().left + 14.142;
   let YtimelineSquare1 = timelineSquare1.offset().top + 32;
 
@@ -19,6 +25,71 @@ let draw = () => {
 
   let XtimelineSquare3 = timelineSquare3.offset().left + 14.142;
   let YtimelineSquare3 = timelineSquare3.offset().top - 7;
+
+  let YtimelineSquare3_1 = timelineSquare3.offset().top + 32;
+
+  let XtimelineSquare4 = timelineSquare4.offset().left + 14.142;
+  let YtimelineSquare4 = timelineSquare4.offset().top + -7;
+  let YtimelineSquare4_1 = timelineSquare4.offset().top + 32;
+
+  let XtimelineSquare5 = timelineSquare5.offset().left + 14.142;
+  let YtimelineSquare5 = timelineSquare5.offset().top + -7;
+  let YtimelineSquare5_1 = timelineSquare5.offset().top + 32;
+
+  let YtimelineSquare6 = timelineSquare6.offset().top;
+
+  var d = `${"M" + XtimelineSquare3 + "," + YtimelineSquare3_1} 
+  ${"L" + XtimelineSquare3 + "," + (YtimelineSquare4 - 100)} 
+  ${
+    "C" +
+    XtimelineSquare3 +
+    "," +
+    (YtimelineSquare4 - 80) +
+    "," +
+    XtimelineSquare3 +
+    "," +
+    (YtimelineSquare4 - 80) +
+    "," +
+    (XtimelineSquare3 + 20) +
+    "," +
+    (YtimelineSquare4 - 80)
+  } 
+  ${"L" + (XtimelineSquare4 - 20) + "," + (YtimelineSquare4 - 80)} 
+  `;
+
+  var d2 = `${"M" + XtimelineSquare4 + "," + YtimelineSquare4_1} 
+  ${"L" + XtimelineSquare4 + "," + (YtimelineSquare4_1 + 300)} 
+  ${
+    "C" +
+    XtimelineSquare4 +
+    "," +
+    (YtimelineSquare4_1 + 320) +
+    "," +
+    XtimelineSquare4 +
+    "," +
+    (YtimelineSquare4_1 + 320) +
+    "," +
+    (XtimelineSquare4 - 20) +
+    "," +
+    (YtimelineSquare4_1 + 320)
+  } 
+  ${"L" + (XtimelineSquare5 - 20) + "," + (YtimelineSquare4_1 + 320)} 
+  ${
+    "C" +
+    XtimelineSquare5 +
+    "," +
+    (YtimelineSquare4_1 + 320) +
+    "," +
+    XtimelineSquare5 +
+    "," +
+    (YtimelineSquare4_1 + 320) +
+    "," +
+    XtimelineSquare5 +
+    "," +
+    (YtimelineSquare4_1 + 340)
+  }
+  ${"L" + XtimelineSquare5 + "," + YtimelineSquare5} 
+  `;
 
   document.getElementById("svg").setAttribute("height", $(document).height());
   $(document.createElementNS("http://www.w3.org/2000/svg", "line"))
@@ -39,12 +110,44 @@ let draw = () => {
       y2: YtimelineSquare3,
     })
     .appendTo("#svg");
+  $(document.createElementNS("http://www.w3.org/2000/svg", "path"))
+    .attr({
+      id: "line4",
+
+      d: d,
+      stroke: "#404E51",
+    })
+    .appendTo("#svg");
+
+  $(document.createElementNS("http://www.w3.org/2000/svg", "path"))
+    .attr({
+      id: "line5",
+
+      d: d2,
+      stroke: "#404E51",
+    })
+    .appendTo("#svg");
+
+  $(document.createElementNS("http://www.w3.org/2000/svg", "line"))
+    .attr({
+      id: "line6",
+      stroke: "#404E51",
+      x1: XtimelineSquare3,
+      y1: YtimelineSquare3_1,
+      x2: XtimelineSquare5,
+      y2: YtimelineSquare5,
+    })
+    .appendTo("#svg");
 };
 
 let redraw = () => {
   let timelineSquare1 = $("#timeline-square1");
   let timelineSquare2 = $("#timeline-square2");
   let timelineSquare3 = $("#timeline-square3");
+
+  let timelineSquare4 = $("#timeline-square4");
+
+  let timelineSquare5 = $("#timeline-square5");
 
   let XtimelineSquare1 = timelineSquare1.offset().left + 14.142;
   let YtimelineSquare1 = timelineSquare1.offset().top + 32;
@@ -57,6 +160,83 @@ let redraw = () => {
 
   let XtimelineSquare3 = timelineSquare3.offset().left + 14.142;
   let YtimelineSquare3 = timelineSquare3.offset().top - 7;
+
+  let YtimelineSquare3_1 = timelineSquare3.offset().top + 32;
+
+  let XtimelineSquare4 = timelineSquare4.offset().left + 14.142;
+  let YtimelineSquare4 = timelineSquare4.offset().top + -7;
+  let YtimelineSquare4_1 = timelineSquare4.offset().top + 32;
+
+  let XtimelineSquare5 = timelineSquare5.offset().left + 14.142;
+  let YtimelineSquare5 = timelineSquare5.offset().top + -7;
+  let YtimelineSquare5_1 = timelineSquare5.offset().top + 32;
+
+  var d = `${"M" + XtimelineSquare3 + "," + YtimelineSquare3_1} 
+  ${"L" + XtimelineSquare3 + "," + (YtimelineSquare4 - 100)} 
+  ${
+    "C" +
+    XtimelineSquare3 +
+    "," +
+    (YtimelineSquare4 - 80) +
+    "," +
+    XtimelineSquare3 +
+    "," +
+    (YtimelineSquare4 - 80) +
+    "," +
+    (XtimelineSquare3 + 20) +
+    "," +
+    (YtimelineSquare4 - 80)
+  } 
+  ${"L" + (XtimelineSquare4 - 20) + "," + (YtimelineSquare4 - 80)} 
+  ${
+    "C" +
+    XtimelineSquare4 +
+    "," +
+    (YtimelineSquare4 - 80) +
+    "," +
+    XtimelineSquare4 +
+    "," +
+    (YtimelineSquare4 - 80) +
+    "," +
+    XtimelineSquare4 +
+    "," +
+    (YtimelineSquare4 - 60)
+  }
+  ${"L" + XtimelineSquare4 + "," + YtimelineSquare4} `;
+
+  var d2 = `${"M" + XtimelineSquare4 + "," + YtimelineSquare4_1} 
+  ${"L" + XtimelineSquare4 + "," + (YtimelineSquare4_1 + 300)} 
+  ${
+    "C" +
+    XtimelineSquare4 +
+    "," +
+    (YtimelineSquare4_1 + 320) +
+    "," +
+    XtimelineSquare4 +
+    "," +
+    (YtimelineSquare4_1 + 320) +
+    "," +
+    (XtimelineSquare4 - 20) +
+    "," +
+    (YtimelineSquare4_1 + 320)
+  } 
+  ${"L" + (XtimelineSquare5 + 20) + "," + (YtimelineSquare4_1 + 320)} 
+  ${
+    "C" +
+    XtimelineSquare5 +
+    "," +
+    (YtimelineSquare4_1 + 320) +
+    "," +
+    XtimelineSquare5 +
+    "," +
+    (YtimelineSquare4_1 + 320) +
+    "," +
+    XtimelineSquare5 +
+    "," +
+    (YtimelineSquare4_1 + 340)
+  }
+  ${"L" + XtimelineSquare5 + "," + YtimelineSquare5} 
+  `;
 
   document.getElementById("svg").setAttribute("height", $(document).height());
   document.getElementById("svg").setAttribute("height", $(document).height());
@@ -72,6 +252,18 @@ let redraw = () => {
     y1: YtimelineSquare2_1,
     x2: XtimelineSquare3,
     y2: YtimelineSquare3,
+  });
+  $("#line4").attr({
+    d: d,
+  });
+  $("#line5").attr({
+    d: d2,
+  });
+  $("#line6").attr({
+    x1: XtimelineSquare3,
+    y1: YtimelineSquare3_1,
+    x2: XtimelineSquare5,
+    y2: YtimelineSquare5,
   });
 };
 

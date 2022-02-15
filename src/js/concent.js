843a8a27eeb610a955f017bb50812ecff9d3394b
Josh Cornau <hi@joshcornau.de>
2022-02-15 08:42:37 +0100

    fixed Cookie Consent

--- src/js/concent.js
+++ src/js/concent.js
@@ -1,5 +1,3 @@
-sessionStorage.setItem("lastname", "Smith");
-
 window.onload = function () {
   if (!sessionStorage.consent) {
     sessionStorage.consent = 10;
@@ -19,10 +17,15 @@ document
   .getElementById("button-accept")
   .addEventListener("click", consentGranted);
 
+document
+  .getElementById("button-discard")
+  .addEventListener("click", consentNotGranted);
+
 function consentGranted() {
   document.getElementById("consent-container").classList.add("notLoaded");
   gtag("consent", "update", {
     ad_storage: "granted",
+    analytics_storage: "granted",
   });
   console.log(window.dataLayer.find((element) => element["consent"]));
   sessionStorage.consent = 20;
